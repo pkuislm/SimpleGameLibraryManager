ae9d76901d69bccb69453f415c103cfbd184b83b
pkuislm <69719051+pkuislm@users.noreply.github.com>
2022-05-18 13:23:25 +0800

    Add DataService for test

---
+++ GameContentService.cs
@@ -0,0 +1,21 @@
+﻿using NetDimension.NanUI.Resource.Data;
+using NetDimension.NanUI.Browser.ResourceHandler;
+
+namespace SimpleGameLibraryManager
+{
+    [DataRoute("/Game")]
+    public class GameContentService : DataService
+    {
+        private int GetGamesCount()
+        {
+            return 114514;
+        }
+
+        [RoutePost("Contents/GetNum")]
+        public ResourceResponse GetNum()
+        {
+            // 返回json
+            return Json(new { success = true, num = GetGamesCount() });
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -20,12 +20,15 @@ class Program
 
         }, app =>
         {
-            //use System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, "Libman") when release
+#if RELEASE
+            app.UseLocalFileResource("https", "static.app.local", System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, "Libman"));
+#else
             app.UseLocalFileResource("https", "static.app.local", @"E:\Visual Studio Project\BigWorks\SimpleGameLibraryManager\Libman");
+#endif
 
             // Register DataServiceResource handler which can process http request and return data to response. It will find all DataServices in current assembly automatically or you can indicate where to find the DataServices by using the third parameter.
             // 注册数据资源控制器，它能处理前端的http请求并返回相应结果。DataServiceResource会自动扫描并注册程序集内的数据服务，您也可以手动指定数据服务所在的位置。
-            //app.UseDataServiceResource("http", "api.app.local");
+            app.UseDataServiceResource("https", "api.app.local");
             
 
             // 指定启动窗体
